f3cff6f1d65192510ca0cc607f14074fde7ed78c
Nathaniel Trainor <natetrainor@msn.com>
2018-03-10 10:01:44 -0700

    Populated AirField.

--- src/com/skilldistillery/projects/jets/AirField.java
+++ src/com/skilldistillery/projects/jets/AirField.java
@@ -4,6 +4,10 @@ public class AirField {
 	private Jet jetFleet[] = new Jet[20];
 	
 	public void airField() {
-		
+		jetFleet[0] = new CargoJet("Great Heron", 79, 400, 82_200_000L);
+		jetFleet[1] = new JetImpl("Skipper", 49, 300, 590_000L);
+		jetFleet[2] = new CargoJet("CAN-722", 129, 700, 218_450_000L);
+		jetFleet[3] = new FighterJet("Lockheed-Martin F-22", 1450, 1800, 78_200_000_000L);
+		jetFleet[4] = new FighterJet("McDonnell Douglas F-15 Eagle", 1850, 2992, 70_000_000L);
 	}
 }
